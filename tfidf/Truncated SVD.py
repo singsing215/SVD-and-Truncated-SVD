0ce6bce80561181a1f5d506fdf3de62c5ea06831
singsing215 <felvex@163.com>
2020-02-22 19:46:51 +0800

    Convert the query to its representation of the reduced dimension

--- tfidf/Truncated SVD.py
+++ tfidf/Truncated SVD.py
@@ -4,14 +4,9 @@ from scipy.linalg import svd
 
 data = xlrd.open_workbook('C:\\Users\\SingSing\\Documents\\GitHub\\SVD-and-Truncated-SVD\\tfidf\\tfidf_result.xlsx')
 table = data.sheets()[0]
-# print(table)
-# nrows = table.nrows 
-# ncols = table.ncols 
-# c1=arange(0,nrows,1)
-# print(c1)
 
 start=1  #start row
-end=5  #end row
+end=6  #end row
  
 rows=end-start
 
@@ -20,18 +15,23 @@ for x in range(start,end):
     values=[]
     row =table.row_values(x)
     for i in range(1,11):
-        # print(value)
         values.append(row[i])
     list_values.append(values)
-# print(list_values)
 X=np.array(list_values)
 print('\n X = \n', X) 
+
 U, s, VT = svd(X)
-
-print('\nU = \n', U) 
-print('s = \n', s) 
-print('VT = \n', VT)
-
+# Truncated SVD: to 2 dim 
 k=2
 recovered_X= U[:,:k].dot(np.diag(s)[:k,:k]).dot(VT [:k, :])
 print("\n X_%d = \n" %k , recovered_X) 
+
+
+
+
+
+
+
+
+
+
