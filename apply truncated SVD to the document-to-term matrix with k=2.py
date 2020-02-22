42be1578fcb8a687c24ba27ca1d3142e077d34e7
singsing215 <felvex@163.com>
2020-02-22 17:21:31 +0800

    create notebook

--- apply truncated SVD to the document-to-term matrix with k=2.py
+++ apply truncated SVD to the document-to-term matrix with k=2.py
@@ -6,19 +6,18 @@ X = np.array([[1, 2, 1, 2], [0, 1, 0, 1], [1, 0, 1, 0], [1, 2, 3, 4] ]) # SVD: X
 U, s, VT = svd(X)
 recovered_X = U.dot(np.diag(s)).dot(VT) 
 print('\n X = \n', X) 
-print('\nU = \n', U) 
-print('s = \n', s) 
-print('VT = \n', VT)
-
 
 # Truncated SVD: to k dim 
-err = [0,0,0] 
-for k in [1,2,3]:
-    recovered_X= U[:,:k].dot(np.diag(s)[:k,:k]).dot(VT [:k, :])
-    print("\n X_%d = \n" %k , recovered_X) 
+#err = [0,0,0] 
+k=2
+recovered_X= U[:,:k].dot(np.diag(s)[:k,:k]).dot(VT [:k, :])
+print("\n X_%d = \n" %k , recovered_X) 
+    
+'''
     print("norm(X,X%d) = %3.2f" %(k,np.linalg.norm(X-recovered_X))) 
     err[k-1] = np.linalg.norm(X-recovered_X);
 pt.plot([1,2,3],err, "ro-") 
 pt.bar([1,2,3],err) 
 pt.xlabel("k - dimension") 
 pt.ylabel("Norm(X,X_k)")
+'''
