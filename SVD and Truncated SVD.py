3bfd997ffeb41f8f203e26b07d51f9c494633e7c
singsing215 <felvex@163.com>
2020-02-22 17:01:12 +0800

    Truncated SVD to 1,2,3 dim

--- SVD and Truncated SVD.py
+++ SVD and Truncated SVD.py
@@ -1,4 +1,5 @@
 import numpy as np 
+import matplotlib.pyplot as pt
 from scipy.linalg import svd 
 X = np.array([[1, 2, 1, 2], [0, 1, 0, 1], [1, 0, 1, 0], [1, 2, 3, 4] ]) # SVD: X = U*s*V^T 
 U, s, VT = svd(X)
@@ -9,3 +10,14 @@ print('s = \n', s)
 print('VT = \n', VT)
 
 
+# Truncated SVD: to k dim 
+err = [0,0,0] 
+for k in [1,2,3]:
+    recovered_X= U[:,:k].dot(np.diag(s)[:k,:k]).dot(VT [:k, :])
+    print("\n X_%d = \n" %k , recovered_X) 
+    print("norm(X,X%d) = %3.2f" %(k,np.linalg.norm(X-recovered_X))) 
+    err[k-1] = np.linalg.norm(X-recovered_X);
+pt.plot([1,2,3],err, "ro-") 
+pt.bar([1,2,3],err) 
+pt.xlabel("k - dimension") 
+pt.ylabel("Norm(X,X_k)")
